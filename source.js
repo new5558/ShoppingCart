cb32e277672ba920059d0447e084bb347c9fc2e5
new5558 <new17353@gmail.com>
2018-12-26 02:31:21 +0700

    added comment to code

--- source.js
+++ source.js
@@ -1,8 +1,12 @@
+// getElementByID
+
 let submit = document.getElementById('enter');
 let input = document.getElementById('userinput');
 let ul = document.querySelector('ul');
 let list = document.querySelectorAll('.list');
 let deleteBtn = document.querySelectorAll('.delete');
+
+// function decalration
 
 const query = () => {
 	list = document.querySelectorAll('.list');
@@ -23,21 +27,23 @@ const addElementToList = () => {
 		let span = document.createElement("span");
 		span.classList.add("list");
 		span.appendChild(document.createTextNode(input.value));
-		li.appendChild(span);
+		li.appendChild(span);  // append : <span class="list">input.value</span>
 
 		let deleteElement = document.createElement("button");
 		deleteElement.classList.add("delete");
 		deleteElement.classList.add("btn");
 		deleteElement.classList.add("btn-dark")
 		deleteElement.appendChild(document.createTextNode("delete"));
-		li.appendChild(deleteElement);
+		li.appendChild(deleteElement); // append : <button class="delete btn btn-dark">delete</button>
 
-		ul.appendChild(li);
+		ul.appendChild(li); //append : <li><span class="list">input.value</span> <button class="delete btn btn-dark">delete</button> </li> 
 		input.value = "";
 		query();
 		addListenerBykey(list.length -1);
 	}
 };
+
+// Main operation
 
 submit.addEventListener("click", addElementToList);
 input.addEventListener("keypress", () => {
